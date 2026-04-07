b448d7ebd3295259a8845bdeeb6b68c2cff2658b
Angelo <elpepechingon@outlook.com>
2026-04-07 14:53:26 -0600

    colores :D

--- internal/colors/colors.go
+++ internal/colors/colors.go
@@ -1,1 +1,28 @@
 package colors
+
+import "fmt"
+
+type Color string
+
+const (
+	Black   Color = "30"
+	Red     Color = "31"
+	Green   Color = "32"
+	Yellow  Color = "33"
+	Blue    Color = "34"
+	Magenta Color = "35"
+	Cyan    Color = "36"
+	White   Color = "37"
+)
+
+func RGB(red int, green int, blue int) Color {
+	return Color(fmt.Sprintf("38;2;%d;%d;%d", red, green, blue))
+}
+
+func Print(input string, color Color) {
+	fmt.Printf("\x1b[%sm%s\x1b[0m", color, input)
+}
+
+func Println(input string, color Color) {
+	Print(input+"\n", color)
+}
